b5aacc4bed09495b27fa6d8732e4fe3231b92435
Roberto Isaac <20220397@itla.edu.do>
2023-02-19 09:37:14 -0600

    Commit 1

---
+++ itlapr/itlapr.DAL/Context/Itla.Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using itlapr.DAL.Entities;
+
+namespace itlapr.DAL.Context
+{
+    public class ItlaContext : DbContext
+    {
+        #region"Registros"
+        public DbSet<Product> Products { get; set; }
+        #endregion""
+    }
+}
---
+++ itlapr/itlapr.DAL/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using itlapr.DAL.Core;
+
+namespace itlapr.DAL.Entities
+{
+    public class Product : ProductN
+    {
+       public int ProductId { get; set; }
+        public int Description { get; set; }
+    }
+}
