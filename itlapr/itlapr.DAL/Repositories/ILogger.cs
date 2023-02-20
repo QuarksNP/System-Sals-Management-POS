a9bba26e52d778c4afdaab1e338b6443dce3c556
Misael Roman <misaelromantjada@gmail.com>
2023-02-19 20:19:55 -0400

    Repositories

---
+++ itlapr/itlapr.DAL/Repositories/ILogger.cs
@@ -0,0 +1,8 @@
+﻿namespace itlapr.DAL.Repositories
+{
+    public interface ILogger<T>
+    {
+        void logError(string v1, Exception ex, string? v2);
+        void logError(string v1, string v2);
+    }
+}
